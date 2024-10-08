58e27048807e9b6b8955cd0895631d3d2952a2af
yacine bouklif <y.bouklif@it-students.fr>
2024-10-08 18:20:04 +0200

    Creation du fichier models pour le schema client

---
+++ ExpressJs/models/client.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+
+ const ClientSchema = mongoose.Schema({
+    nom: {
+        type: String,
+        required: [true, 'Le nom est requis'],
+        minlength: [3, 'Le nom doit avoir au moins 3 caractères'],
+        maxlength: [30, 'Le nom ne doit pas dépasser 30 caractères'],
+        trim: true,
+        validate: {
+            validator: v => /.*[a-zA-Z].*/.test(v),
+            message: "Le nom doit contenir au moins une lettre"
+        }
+    },
+    isGold: {
+        type: Boolean,
+        default: false
+    },
+    phone: {
+        type: String,
+        required: [true, 'Le numéro de téléphone est requis'],
+        validate: {
+            validator: function(v) {
+                return /^\d{10}$/.test(v);
+            },
+            message: props => `${props.value} n'est pas un numéro de téléphone valide! Il doit contenir exactement 10 chiffres.`
+        }
+    }
+});
+
+export const Client = mongoose.model('Client', ClientSchema);
+
+
+export const SchemaClient = Joi.object({
+    nom: Joi.string().min(3).max(30).regex(/.*[a-zA-Z].*/).trim().required()
+        .messages({
+            'string.pattern.base': 'Le nom doit contenir au moins une lettre',
+            'string.min': 'Le nom doit avoir au moins {#limit} caractères',
+            'string.max': 'Le nom ne doit pas dépasser {#limit} caractères',
+            'any.required': 'Le nom est requis'
+        }),
+
+    isGold: Joi.boolean().default(false),
+    phone: Joi.string().length(10).pattern(/^[0-9]+$/).required()
+        .messages({
+            'string.length': 'Le numéro de téléphone doit avoir exactement {#limit} chiffres',
+            'string.pattern.base': 'Le numéro de téléphone ne doit contenir que des chiffres',
+            'any.required': 'Le numéro de téléphone est requis'
+        })
+});
--- ExpressJs/routes/clients.js
+++ ExpressJs/routes/clients.js
@@ -1,57 +1,8 @@
 import express from 'express';
 import mongoose from "mongoose";
-import Joi from "joi";
+import {Client,SchemaClient} from "../models/client.js"
 
 const router = express.Router();
-
-
-const SchemaClient = Joi.object({
-    nom: Joi.string().min(3).max(30).regex(/.*[a-zA-Z].*/).trim().required()
-        .messages({
-            'string.pattern.base': 'Le nom doit contenir au moins une lettre',
-            'string.min': 'Le nom doit avoir au moins {#limit} caractères',
-            'string.max': 'Le nom ne doit pas dépasser {#limit} caractères',
-            'any.required': 'Le nom est requis'
-        }),
-
-    isGold: Joi.boolean().default(false),
-    phone: Joi.string().length(10).pattern(/^[0-9]+$/).required()
-        .messages({
-            'string.length': 'Le numéro de téléphone doit avoir exactement {#limit} chiffres',
-            'string.pattern.base': 'Le numéro de téléphone ne doit contenir que des chiffres',
-            'any.required': 'Le numéro de téléphone est requis'
-        })
-});
-
-const ClientSchema = mongoose.Schema({
-    nom: {
-        type: String,
-        required: [true, 'Le nom est requis'],
-        minlength: [3, 'Le nom doit avoir au moins 3 caractères'],
-        maxlength: [30, 'Le nom ne doit pas dépasser 30 caractères'],
-        trim: true,
-        validate: {
-            validator: v => /.*[a-zA-Z].*/.test(v),
-            message: "Le nom doit contenir au moins une lettre"
-        }
-    },
-    isGold: {
-        type: Boolean,
-        default: false
-    },
-    phone: {
-        type: String,
-        required: [true, 'Le numéro de téléphone est requis'],
-        validate: {
-            validator: function(v) {
-                return /^\d{10}$/.test(v);
-            },
-            message: props => `${props.value} n'est pas un numéro de téléphone valide! Il doit contenir exactement 10 chiffres.`
-        }
-    }
-});
-
-const Client = mongoose.model('Client', ClientSchema);
 
 router.get('/', async (req, res) => {
     const clients = await Client.find().sort("nom");
@@ -89,18 +40,24 @@ router.put("/:id", async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
         return res.status(400).send("ID invalide");
     }
+
     const validationResult = SchemaClient.validate(req.body);
     if (validationResult.error) {
         return res.status(400).send(validationResult.error.details[0].message);
-    }    
+    }
+    
     try {
         const client = await Client.findByIdAndUpdate(
             req.params.id,
-            { nom: req.body.nom },
-            { phone: req.body.phone,},
+            { 
+                nom: req.body.nom,
+                phone: req.body.phone,
+                isGold: req.body.isGold
+            },
             { new: true, runValidators: true }
         );
-        if (!client) return res.status(404).send("Genre non trouvé");
+
+        if (!client) return res.status(404).send("Client non trouvé");
         res.send(client);
     } catch (error) {
         if (error instanceof mongoose.Error.ValidationError) {
